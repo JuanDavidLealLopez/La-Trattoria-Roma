6c6284b10eef37cad8291901dc1ba20e80bb874c
JuanDavidLealLopez <juandavidleallopez045@gmail.com>
2026-03-03 20:20:16 -0500

    chore: se agfrega seccion de menu y se avanza un poco en el desarrollo

--- Js/NavScroll.js
+++ Js/NavScroll.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", function () {
   const nav = document.querySelector(".HeaderNav");
 
-  if (window.scrollY > 90) {
+  if (window.scrollY > 520) {
     nav.classList.add("scrolled");
   } else {
     nav.classList.remove("scrolled");
